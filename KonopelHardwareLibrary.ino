4c9f57a058a775cd89e206f879fb5392476a3a60
Ilya Shishov <koteskateboarder@gmail.com>
2018-10-18 16:22:10 +0300

    test sketch

--- KonopelHardwareLibrary.ino
+++ KonopelHardwareLibrary.ino
@@ -1,20 +1,43 @@
-#include "src/Magnet.h"
-//#include "Adafruit_PWMServoDriver.h"
+#include "src/Konopel.h"
 
-Magnet m(15);
+int counter = 0;
+
+bool magnetOn = false;
+
+void onPedalClick()
+{
+    Serial.println("PEDAL");
+    if(Switch.isUp())
+    {
+        magnet.on();
+        
+    }
+}
+
+void onSwitchClick()
+{
+    magnet.off();
+}
 
 void setup()
 {
-    Serial.begin(9600);
-    delay(5000);
+    Serial.begin(115200);
     Serial.println("STARTED");
-    m.begin();
+
+    Switch.onClick(onSwitchClick);
+    Pedal.onClick(onPedalClick);
+    magnet.begin();
 }
 
 void loop()
 {
-    
-    for (int j = 30; j < 100; j++)
+    Pedal.check();
+    Switch.check();
+
+    int val = Pot.read();
+    magnet.write(map(val, 0, 1023, 0, 4095));
+
+    /*for (int j = 30; j < 100; j++)
     {
         for (int i = 0; i < 4096; i += 8)
         {
@@ -22,5 +45,5 @@ void loop()
             m.write(i);
             delay(10);
         }
-    }
+    }*/
 }
